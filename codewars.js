f900de7855299d899c8fd7dba3fe385fa62e4bc0
Volodymyr Pivtoranis <98724271+PivtoranisV@users.noreply.github.com>
2022-05-25 07:45:01 +0400

    Solution for Find the smallest integer in the array

--- codewars.js
+++ codewars.js
@@ -105,3 +105,11 @@ function warnTheSheep(queue) {
 //https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
 const circleCircumference = (circle) => circle.radius * 2 * Math.PI;
+
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
